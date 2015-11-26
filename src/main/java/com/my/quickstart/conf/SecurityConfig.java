dd625cf58181804a1390324147711c199c01073b
kangshaoxiong <892885829@qq.com>
2015-11-26 18:16:00 +0800

    添加 spring security配置注释

--- src/main/java/com/my/quickstart/conf/SecurityConfig.java
+++ src/main/java/com/my/quickstart/conf/SecurityConfig.java
@@ -29,6 +29,7 @@ import com.my.quickstart.security.UserDetailsServiceImpl;
 public class SecurityConfig{
 	
 	@Configuration
+	//设置拦截/适配器 的执行顺序（值越小优先级越高）
 	@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 	protected static class ApplicationSecurity extends WebSecurityConfigurerAdapter {
 		protected final Log logger = LogFactory.getLog(getClass());
@@ -41,6 +42,10 @@ public class SecurityConfig{
 	        auth.authenticationProvider(daoAuthenticationProvider());
 	    }  
 	    
+	    /**
+	     * 获取操作提示文件的路径
+	     * @return
+	     */
 	    public MessageSource getMessageSource(){
 	    	ReloadableResourceBundleMessageSource source=new ReloadableResourceBundleMessageSource();
 	    	source.setBasenames("classpath:/message/security_messages","classpath:org/springframework/security/messages");
@@ -48,28 +53,49 @@ public class SecurityConfig{
 	    	return source;
 	    }
 		
+	    /**
+	     * 身份认证提供程序
+	     * @return
+	     */
 		public DaoAuthenticationProvider daoAuthenticationProvider(){
 			PasswordEncoder encoder=getPasswordEncoder();
 			DaoAuthenticationProvider provider=new DaoAuthenticationProvider();
 			provider.setMessageSource(getMessageSource());
 			provider.setUserDetailsService(userDetailsService);
+			//设置密码加密规则
 			provider.setPasswordEncoder(encoder);
 			provider.setHideUserNotFoundExceptions(false);
 			return provider;
 		}
 		
+		/**
+		 * 登陆成功处理
+		 * @return
+		 */
 		public LoginSuccessHandler loginSuccessHandler(){
 			return new LoginSuccessHandler();
 		}
 		
+		/**
+		 * 退出成功处理
+		 * @return
+		 */
 		public LogoutSuccessHandler logoutSuccessHandler(){
 			return new LogoutSuccessHandler();
 		}
 		
+		/**
+		 * 登陆失败处理
+		 * @return
+		 */
 		public LoginFailHandler loginFailHandler(){
 			return new LoginFailHandler();
 		}
 		
+		/**
+		 * 密码编码器
+		 * @return
+		 */
 		public PasswordEncoder getPasswordEncoder(){
 			return new BCryptPasswordEncoder(Constant.BCRYPT_LENGTH);
 		}
@@ -77,21 +103,34 @@ public class SecurityConfig{
 		protected void configure(HttpSecurity http) throws Exception {
 			http
 	        .authorizeRequests()
+	            //设置不拦截规则
 	            .antMatchers("/static/**","/robots.txt").permitAll()                
 	            .antMatchers("/admin/**").hasRole("ADMIN") 
-	            .antMatchers("/userAccount/**").hasAnyAuthority("ROLE_USER")
+	            .antMatchers("/userAccount/**")
+	            //只要有里面设置的任意一个角色/权限
+	            .hasAnyAuthority("ROLE_USER")
 	            .anyRequest()
-	            .authenticated()                                                   
+	            //认证
+	            .authenticated() 
+	            
 	            .and()
 				.formLogin()
+				//自定义登陆页面
 				.loginPage("/login").permitAll()
+				//默认登陆成功的路径
 				.defaultSuccessUrl("/")
+				//登陆成功后执行的操作
 				.successHandler(loginSuccessHandler())
+				//登陆失败后执行的操作
 				.failureHandler(loginFailHandler())
+				
 				.and()
 				.logout()
+				//退出成功后的路径
 				.logoutSuccessUrl("/login").permitAll()
+				//退出成功后执行的操作
 				.logoutSuccessHandler(logoutSuccessHandler())
+	
 				.and().httpBasic();
 		}
 	}
